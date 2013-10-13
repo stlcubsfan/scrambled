176b3769b27d2c839a85acab7d72ff4d5a1ec2c1
Brian Coles <stlcubsfan@gmail.com>
2013-10-13 15:04:54 -0500

    initial stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitzer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:sign_up) << :name
   end
 end
