4f5b5e63fa7c8186c6eb15c39064ccdf3b8cfb14
Brian Coles <stlcubsfan@gmail.com>
2013-10-13 13:33:33 -0500

    removing attr_accessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,5 +46,5 @@ class User
   index({ email: 1 }, { unique: true, background: true })
   field :name, :type => String
   validates_presence_of :name
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :created_at, :updated_at
+  #attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :created_at, :updated_at
 end
